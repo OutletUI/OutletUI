66fba6e072a9510b0746e23a8abfeda65383f3ab
Roni Sarkar <ronichandrasarkar@gmail.com>
2026-02-20 13:00:58 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div></div>;
+  return <div>
+     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+      Button
+    </button>
+  </div>;
 }
